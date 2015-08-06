660243dc918ff25303f024ecf9e5f1a9bb8deaa6
icarus <icarus@icarusdeMacBook-Pro.local>
2015-08-06 15:02:43 +0800

    Initial Commit

---
+++ BBQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBQ
+//
+//  Created by icarus on 15/8/6.
+//  Copyright (c) 2015年 icarus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
